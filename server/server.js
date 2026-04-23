ab19ad3c5342dd4074eb9a145ccc32771a1ed9c8
kartik <kartikrai01017@gmail.com>
2026-04-23 23:28:18 +0530

    cors full access

--- server/server.js
+++ server/server.js
@@ -4,12 +4,7 @@ const cors = require('cors');
 const app = express();
 
 // ✅ CORS setup
-app.use(cors({
-  origin: "https://idea-lab-navy.vercel.app",
-  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-  allowedHeaders: ["Content-Type", "Authorization"],
-  credentials: true
-}));
+app.use(cors());
 
 
 
