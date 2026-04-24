c270b8cd2b36106fa50505a84c4ba384a5a17d77
kartik <kartikrai01017@gmail.com>
2026-04-24 11:22:10 +0530

    changed the site domain in cors

--- server/server.js
+++ server/server.js
@@ -3,9 +3,9 @@ const cors = require('cors');
 
 const app = express();
 
-// ✅ CORS setup
+// ✅ CORS setup https://idea-r3afoivw8-kartikrai01017s-projects.vercel.app/
 const corsOptions = {
-  origin: ["https://idea-awidzrd0n-kartikrai01017s-projects.vercel.app", "http://localhost:5173"],
+  origin: ["https://idea-r3afoivw8-kartikrai01017s-projects.vercel.app/", "http://localhost:5173"],
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization"],
   credentials: true
