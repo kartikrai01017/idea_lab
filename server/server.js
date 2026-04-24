ea4234d2560c2abcf24d3099be57ac00db6dd3bb
kartik <kartikrai01017@gmail.com>
2026-04-24 10:47:33 +0530

    changed .env also cors and dployment ready files

--- server/server.js
+++ server/server.js
@@ -5,7 +5,7 @@ const app = express();
 
 // ✅ CORS setup
 const corsOptions = {
-  origin: ["https://idea-lab-navy.vercel.app", "http://localhost:5173"],
+  origin: ["https://idea-awidzrd0n-kartikrai01017s-projects.vercel.app", "http://localhost:5173"],
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization"],
   credentials: true
